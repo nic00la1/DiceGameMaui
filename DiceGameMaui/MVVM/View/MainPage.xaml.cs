eefef10bf57b5afe1beaab5fec2ffab9566dbf49
nic00la1 <nic00la.kaleta@gmail.com>
2025-05-13 10:33:16 +0200

    dodano animacje puls, przy wybieraniu

--- DiceGameMaui/MVVM/View/MainPage.xaml.cs
+++ DiceGameMaui/MVVM/View/MainPage.xaml.cs
@@ -1,11 +1,22 @@
-﻿namespace DiceGameMaui
+﻿using DiceGameMaui.MVVM.ViewModel;
+using System.ComponentModel;
+
+namespace DiceGameMaui
 {
     public partial class MainPage : ContentPage
     {
-
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void OnDiceTapped(object sender, TappedEventArgs e)
+        {
+            if (sender is Frame frame)
+            {
+                await frame.ScaleTo(1.2, 100, Easing.CubicIn);
+                await frame.ScaleTo(1.0, 100, Easing.CubicOut);
+            }
         }
     }
 
